d62ed6dccb0804c2e89717e18c09e8ffa61aac20
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-15 12:19:23 -0300

    Add files via upload

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -15,35 +15,43 @@ st.title("Student Performance Analysis")
 st.subheader("Preview of the Data:")
 st.write(df_student_math.head())
 
+# Filter by Age
+selected_age = st.slider("Select Age:", min_value=int(df_student_math['age'].min()), max_value=int(df_student_math['age'].max()))
+filtered_data_by_age = df_student_math[df_student_math['age'] == selected_age]
+
+# Display filtered data
+st.subheader(f"Data for Students with Age {selected_age}:")
+st.write(filtered_data_by_age)
+
 # Average ages by school
 average_age_by_school = df_student_math.groupby('school')['age'].mean()
 st.subheader("Average Ages by School:")
 st.bar_chart(average_age_by_school)
 
 # Average final grades by different groups
-groups_to_analyze = ['parent_status', 'address_type', 'age', 'family_size', 'study_time']
+groups_to_analyze = ['parent_status', 'address_type', 'family_size', 'study_time']
 
 for group in groups_to_analyze:
-    average_final_grade_by_group = df_student_math.groupby(group)['final_grade'].mean()
-    st.subheader(f"Average Final Grade by {group}:")
+    average_final_grade_by_group = filtered_data_by_age.groupby(group)['final_grade'].mean()
+    st.subheader(f"Average Final Grade by {group} for Age {selected_age}:")
     st.bar_chart(average_final_grade_by_group)
 
 # Scatter plot
-st.subheader("Scatter Plot: Health vs. Social with Bubble Size as Final Grade")
+st.subheader("Scatter Plot: Health vs. Social with Bubble Size as Final Grade for Age {selected_age}")
 scatter_plot_fig = px.scatter(
-    df_student_math,
+    filtered_data_by_age,
     x='health',
     y='social',
     size='final_grade',
     color='free_time',
-    title='Health vs. Social with Bubble Size as Final Grade'
+    title=f'Health vs. Social with Bubble Size as Final Grade for Age {selected_age}'
 )
 st.plotly_chart(scatter_plot_fig)
 
 # Summary statistics
-st.subheader("Summary Statistics:")
-selected_columns = st.multiselect("Select Columns for Summary Statistics:", df_student_math.columns)
+st.subheader(f"Summary Statistics for Age {selected_age}:")
+selected_columns = st.multiselect("Select Columns for Summary Statistics:", filtered_data_by_age.columns)
 if selected_columns:
-    st.write(df_student_math[selected_columns].describe())
+    st.write(filtered_data_by_age[selected_columns].describe())
 else:
     st.warning("Please select at least one column for summary statistics.")
