9a30d8196c479bd1e54c2e708c00c955f0073089
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-15 12:22:51 -0300

    Add files via upload

--- Informe.py
+++ Informe.py
@@ -1,52 +1,65 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import plotly.express as px
 
-# Link para o conjunto de dados no Kaggle
-dataset_link = "https://www.kaggle.com/datasets/dillonmyrick/high-school-student-performance-and-demographics"
+# Load data from CSV
+file_path = "student_math_clean.csv"  # Replace with the actual file path
+try:
+    df_student_math = pd.read_csv(file_path)
+except FileNotFoundError:
+    st.error(f"File not found: {file_path}")
+    st.stop()
 
-df_student_performance = pd.read_csv('student_math_clean.csv')
+# Display the data
+st.title("Student Performance Analysis")
+st.subheader("Preview of the Data:")
+st.write(df_student_math.head())
 
-st.title("Análise de Desempenho dos Alunos")
-st.write(
-    "Esta análise utiliza dados sobre o desempenho dos alunos. "
-    "Os dados contêm informações sobre o ID do aluno, escola, sexo, idade, tipo de endereço, tamanho da família, "
-    "estado dos pais, educação da mãe, educação do pai, ocupação da mãe, ocupação do pai, motivo de escolha da escola, "
-    "guardião, tempo de viagem, tempo de estudo, falhas nas notas, apoio da escola, apoio da família, aulas extras pagas, "
-    "atividades extracurriculares, frequência à creche, aspiração para ensino superior, acesso à internet, relacionamento romântico, "
-    "relacionamento familiar, tempo livre, interação social, consumo de álcool durante a semana, consumo de álcool no fim de semana, "
-    "saúde, faltas, nota no primeiro período, nota no segundo período, nota final."
+# Filters
+selected_school = st.selectbox("Select School:", df_student_math['school'].unique())
+selected_family_size = st.selectbox("Select Family Size:", df_student_math['family_size'].unique())
+selected_address_type = st.selectbox("Select Address Type:", df_student_math['address_type'].unique())
+
+# Apply filters
+filtered_data = df_student_math[
+    (df_student_math['school'] == selected_school) &
+    (df_student_math['family_size'] == selected_family_size) &
+    (df_student_math['address_type'] == selected_address_type)
+]
+
+# Display filtered data
+st.subheader("Filtered Data:")
+st.write(filtered_data)
+
+# Average ages by school
+average_age_by_school = filtered_data.groupby('school')['age'].mean()
+st.subheader("Average Ages by School:")
+st.bar_chart(average_age_by_school)
+
+# Average final grades by different groups
+groups_to_analyze = ['parent_status', 'study_time']
+
+for group in groups_to_analyze:
+    average_final_grade_by_group = filtered_data.groupby(group)['final_grade'].mean()
+    st.subheader(f"Average Final Grade by {group}:")
+    st.bar_chart(average_final_grade_by_group)
+
+# Scatter plot
+st.subheader("Scatter Plot: Health vs. Social with Bubble Size as Final Grade")
+scatter_plot_fig = px.scatter(
+    filtered_data,
+    x='health',
+    y='social',
+    size='final_grade',
+    color='free_time',
+    title='Health vs. Social with Bubble Size as Final Grade'
 )
+st.plotly_chart(scatter_plot_fig)
 
-total_tables = 1
-total_columns = df_student_performance.shape[1]
-total_students = df_student_performance.shape[0]
-
-st.subheader("Informações Gerais")
-st.write(f"Total de tabelas: {total_tables}")
-st.write(f"Total de colunas na tabela: {total_columns}")
-st.write(f"Total de alunos: {total_students}")
-
-st.header("Dados Completos de Desempenho dos Alunos")
-st.write(df_student_performance)
-
-# Separando por séries
-st.header("Análise por Séries")
-series = df_student_performance['school'].unique()
-serie_selecionada = st.selectbox("Selecione uma série:", series)
-
-df_serie_selecionada = df_student_performance[df_student_performance['school'] == serie_selecionada]
-
-st.subheader(f"Informações sobre a Série {serie_selecionada}")
-st.write(df_serie_selecionada)
-
-# Adicionando um pouco sobre a importância da educação
-st.header("Importância da Educação:")
-st.write(
-    "A educação é uma ferramenta crucial para o desenvolvimento humano. "
-    "O desempenho dos alunos reflete não apenas o aprendizado individual, mas também o sistema educacional como um todo. "
-    "Investir na educação é investir no futuro, proporcionando oportunidades para o crescimento pessoal e profissional."
-)
-
-# Adicionando link para o Kaggle
-st.subheader("Conjunto de Dados no Kaggle:")
-st.write(f"O conjunto de dados foi obtido no Kaggle. Você pode acessá-lo [aqui]({dataset_link}).")
+# Summary statistics
+st.subheader("Summary Statistics:")
+selected_columns = st.multiselect("Select Columns for Summary Statistics:", filtered_data.columns)
+if selected_columns:
+    st.write(filtered_data[selected_columns].describe())
+else:
+    st.warning("Please select at least one column for summary statistics.")
